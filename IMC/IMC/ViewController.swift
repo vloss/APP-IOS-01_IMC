65897ba8bdcc7884b4852c7debc8a42715339ad3
vloss <viniciusvianaloss@gmail.com>
2022-12-26 21:28:22 -0300

    Finalização de alinhamneto de tela e inclusão de variaveis e metodo calcular na ViewController.

--- IMC/IMC/ViewController.swift
+++ IMC/IMC/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nr_peso: UITextField!
+    @IBOutlet weak var nr_altura: UITextField!
+    @IBOutlet weak var txt_resultado: UILabel!
+    @IBOutlet weak var img_resultado: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn_calcular(_ sender: Any) {
+    }
+    
 }
 
